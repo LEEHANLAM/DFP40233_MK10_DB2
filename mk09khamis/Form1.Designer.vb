663acf4f9168ae6a5b1a6f6d16dcf64c66d982c8
ASUS TUF <ASUS TUF@LAPTOP-AHT9UBFA>
2024-10-14 12:59:13 +0800

    Add project files.

---
+++ mk09khamis/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        txtIdKelas = New TextBox()
+        txtNamaKelas = New TextBox()
+        btnAddKelas = New Button()
+        btnUpdateKelas = New Button()
+        btnDeleteKelas = New Button()
+        dataGridViewKelas = New DataGridView()
+        Timer1 = New Timer(components)
+        CType(dataGridViewKelas, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtIdKelas
+        ' 
+        txtIdKelas.Location = New Point(20, 23)
+        txtIdKelas.Name = "txtIdKelas"
+        txtIdKelas.PlaceholderText = "Enter ID Kelas"
+        txtIdKelas.Size = New Size(194, 27)
+        txtIdKelas.TabIndex = 0
+        ' 
+        ' txtNamaKelas
+        ' 
+        txtNamaKelas.Location = New Point(20, 63)
+        txtNamaKelas.Name = "txtNamaKelas"
+        txtNamaKelas.PlaceholderText = "Enter Nama Kelas"
+        txtNamaKelas.Size = New Size(194, 27)
+        txtNamaKelas.TabIndex = 1
+        ' 
+        ' btnAddKelas
+        ' 
+        btnAddKelas.Location = New Point(255, 21)
+        btnAddKelas.Name = "btnAddKelas"
+        btnAddKelas.Size = New Size(122, 30)
+        btnAddKelas.TabIndex = 2
+        btnAddKelas.Text = "Add"
+        btnAddKelas.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdateKelas
+        ' 
+        btnUpdateKelas.Location = New Point(255, 61)
+        btnUpdateKelas.Name = "btnUpdateKelas"
+        btnUpdateKelas.Size = New Size(122, 30)
+        btnUpdateKelas.TabIndex = 3
+        btnUpdateKelas.Text = "Update"
+        btnUpdateKelas.UseVisualStyleBackColor = True
+        ' 
+        ' btnDeleteKelas
+        ' 
+        btnDeleteKelas.Location = New Point(255, 101)
+        btnDeleteKelas.Name = "btnDeleteKelas"
+        btnDeleteKelas.Size = New Size(122, 30)
+        btnDeleteKelas.TabIndex = 4
+        btnDeleteKelas.Text = "Delete"
+        btnDeleteKelas.UseVisualStyleBackColor = True
+        ' 
+        ' dataGridViewKelas
+        ' 
+        dataGridViewKelas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dataGridViewKelas.Location = New Point(20, 150)
+        dataGridViewKelas.Name = "dataGridViewKelas"
+        dataGridViewKelas.RowHeadersWidth = 51
+        dataGridViewKelas.Size = New Size(357, 221)
+        dataGridViewKelas.TabIndex = 5
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 5000
+        ' 
+        ' Form1
+        ' 
+        ClientSize = New Size(389, 379)
+        Controls.Add(dataGridViewKelas)
+        Controls.Add(btnDeleteKelas)
+        Controls.Add(btnUpdateKelas)
+        Controls.Add(btnAddKelas)
+        Controls.Add(txtNamaKelas)
+        Controls.Add(txtIdKelas)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Kelas Management"
+        CType(dataGridViewKelas, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtIdKelas As TextBox
+    Friend WithEvents txtNamaKelas As TextBox
+    Friend WithEvents btnAddKelas As Button
+    Friend WithEvents btnUpdateKelas As Button
+    Friend WithEvents btnDeleteKelas As Button
+    Friend WithEvents dataGridViewKelas As DataGridView
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ mk09khamis/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private conn As MySqlConnection
+
+    Private Sub FormKelas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn = New MySqlConnection("Server=172.20.10.3;Database=mk9;Uid=username;Pwd=password;")
+        LoadDataKelas()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        LoadDataKelas() ' Refresh the data every tick (every minute)
+    End Sub
+
+    Private Sub LoadDataKelas()
+        Dim query As String = "SELECT * FROM kelas"
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+        Dim adapter As New MySqlDataAdapter(cmd)
+        Dim table As New DataTable()
+
+        Try
+            conn.Open()
+            adapter.Fill(table)
+            dataGridViewKelas.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnAddKelas_Click(sender As Object, e As EventArgs) Handles btnAddKelas.Click
+        Dim query As String = "INSERT INTO kelas (idkelas, namakelas) VALUES (@idkelas, @namakelas)"
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@idkelas", txtIdKelas.Text)
+        cmd.Parameters.AddWithValue("@namakelas", txtNamaKelas.Text)
+
+        Try
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Kelas added successfully.")
+            LoadDataKelas()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnUpdateKelas_Click(sender As Object, e As EventArgs) Handles btnUpdateKelas.Click
+        Dim query As String = "UPDATE kelas SET namakelas = @namakelas WHERE idkelas = @idkelas"
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@idkelas", txtIdKelas.Text)
+        cmd.Parameters.AddWithValue("@namakelas", txtNamaKelas.Text)
+
+        Try
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Kelas updated successfully.")
+            LoadDataKelas()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnDeleteKelas_Click(sender As Object, e As EventArgs) Handles btnDeleteKelas.Click
+        Dim query As String = "DELETE FROM kelas WHERE idkelas = @idkelas"
+        Dim cmd As MySqlCommand = New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@idkelas", txtIdKelas.Text)
+
+        Try
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Kelas deleted successfully.")
+            LoadDataKelas()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub dataGridViewKelas_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dataGridViewKelas.CellClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = dataGridViewKelas.Rows(e.RowIndex)
+            txtIdKelas.Text = row.Cells("idkelas").Value.ToString()
+            txtNamaKelas.Text = row.Cells("namakelas").Value.ToString()
+        End If
+    End Sub
+
+
+End Class
